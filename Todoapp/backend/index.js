c88503ad519aec86ba1ae71e8437f69458fa0b66
Kaustubh <kaustubhjaitapkar2004@gmail.com>
2024-11-12 23:00:45 +0530

    schema

--- Todoapp/backend/index.js
+++ Todoapp/backend/index.js
@@ -31,9 +31,10 @@ app.listen(PORT, () => {
 
 /*
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  password VARCHAR(255)
 );
 
 CREATE TABLE tasks (
